85f6a2a410cb8c346ddab4710f622abd5025e614
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-09 22:45:24 +0900

    feat: typescript導入

---
+++ src/Home.tsx
@@ -0,0 +1,45 @@
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+function SignInButton() {
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider);
+  };
+
+  return <button onClick={signInWithGoogle}>Googleでサインイン</button>;
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>サインアウト</button>;
+}
+
+function UserInfo() {
+  if (!auth.currentUser) return null;
+
+  return (
+    <div className="UserInfo">
+      <img src={auth.currentUser.photoURL || undefined} alt=""></img>
+      <p>{auth.currentUser.displayName}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div>
+      {user ? (
+        <>
+          <UserInfo />
+          <SignOutButton />
+        </>
+      ) : (
+        <SignInButton />
+      )}
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFEfWFLmdtefcaaH1Dw6UBNsHlLZePZPs",
+  authDomain: "fir-signin-99989.firebaseapp.com",
+  projectId: "fir-signin-99989",
+  storageBucket: "fir-signin-99989.appspot.com",
+  messagingSenderId: "605975442962",
+  appId: "1:605975442962:web:afd31b1be411806b4e8acb",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
