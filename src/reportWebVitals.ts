7afa7a26b3170603678d3e54bdebf7b5180cecfa
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-09 22:46:20 +0900

    fix: jsファイルをtsxに変更

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,6 +1,8 @@
-const reportWebVitals = onPerfEntry => {
+type perEntryHandler = (metric: any) => void;
+
+const reportWebVitals = (onPerfEntry: perEntryHandler | undefined) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
