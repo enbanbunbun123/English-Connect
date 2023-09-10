f22eb295ab4fa4ed35ae10e4cbade724f009191b
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-10 15:29:01 +0900

    fix: ヘッダーのスタイルを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { auth } from "../firebase";
 import "../stylesheet/header.scss";
+import SignOutButton from "./SignOutButton ";
 
 const Header: React.FC = () => {
   if (!auth.currentUser) return null;
@@ -8,6 +9,7 @@ const Header: React.FC = () => {
   return (
     <>
       <div className="Header">
+        <SignOutButton />
         <div className="Header__user-info">
           <img src={auth.currentUser.photoURL || undefined} alt=""></img>
           <p>{auth.currentUser.displayName}</p>
