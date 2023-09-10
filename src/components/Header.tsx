773ea01fdbe52855213be172b3907e845340994c
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-10 13:14:21 +0900

    feat: sass導入

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { auth } from "../firebase";
+import "../stylesheet/header.scss";
 
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
+  if (!auth.currentUser) return null;
+
   return (
     <>
-      <div></div>
+      <div className="Header">
+        <div className="Header__user-info">
+          <img src={auth.currentUser.photoURL || undefined} alt=""></img>
+        </div>
+      </div>
     </>
   );
 };
