ad4b5b94982eb0ef293d734e43275bd6c3f6aa89
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-12 00:56:48 +0900

    feat: firebaseのdbにuserの情報を追加するロジック形成

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,10 +1,27 @@
 import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from "../firebase";
+import { app, auth, provider } from "../firebase";
+import { getDatabase, ref, set } from "firebase/database";
 import "../stylesheet/signInButton.scss";
 
 const SignInButton: React.FC = () => {
   const signInWithGoogle = () => {
-    signInWithPopup(auth, provider);
+    const db = getDatabase(app);
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        const uid = user.uid;
+        const email = user.email;
+
+        const userRef = ref(db, "users/" + uid);
+        set(userRef, {
+          id: uid,
+          gmail: email,
+        });
+      })
+      .catch((error) => {
+        console.error("Error signing in with Google: ", error);
+      });
   };
 
   return (
