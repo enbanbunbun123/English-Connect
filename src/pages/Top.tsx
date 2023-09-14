6eb5ef67f2ed38df425b3a4853e515aedaeb588c
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-15 00:04:23 +0900

    feat: 投稿したものをUIに反映

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,14 +1,45 @@
 import { auth } from "../firebase";
 import "../stylesheet/top.scss";
 import PostForm from "../components/PostForm";
+import { useEffect, useState } from "react";
+import { getDatabase, off, onValue, ref, set } from "firebase/database";
 
 const Top: React.FC = () => {
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    const db = getDatabase();
+    const postsRef = ref(db, "posts");
+
+    const unsubscribe = onValue(postsRef, (snapshot) => {
+      const data = snapshot.val();
+      const loadedPosts = [];
+      for (let id in data) {
+        loadedPosts.push({ id, ...data[id] });
+      }
+      setPosts(loadedPosts);
+    });
+
+    return () => {
+      off(postsRef, "value", unsubscribe);
+    };
+  }, []);
+
   if (!auth.currentUser) return null;
 
   return (
     <>
       <div className="Top">
         <PostForm />
+        <div className="posts">
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <h3>{post.userName}</h3>
+              <p>{post.text}</p>
+              <span>{post.timestamp}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
