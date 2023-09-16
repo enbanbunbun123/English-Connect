9c73568a39997704b96d9ac04c42a2a463300c32
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-16 22:37:40 +0900

    fix: landingページの文言を変更

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,7 +5,7 @@ const Landing: React.FC = () => {
   return (
     <>
       <div className="Landing">
-        <div className="Landing__title">Welcome to my site!</div>
+        <div className="Landing__title">Welcome to English Connect!</div>
         <SignInButton />
       </div>
     </>
