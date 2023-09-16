f8d68644581d5927c1328c317e74a67f181a96a2
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-17 00:49:27 +0900

    feat: ItemDetailページから投稿の削除ができるように修正

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -1,11 +1,25 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
+import { getDatabase, ref, remove } from "firebase/database";
 
 const ItemDetail: React.FC = () => {
   const navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
 
   const navigateToTop = () => {
     navigate("/");
+  };
+
+  const handleDelete = () => {
+    const db = getDatabase();
+    const postRef = ref(db, `posts/${id}`);
+    remove(postRef)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Error deleting post:", error);
+      });
   };
 
   return (
@@ -14,6 +28,7 @@ const ItemDetail: React.FC = () => {
       <button className="PostForm__back-button" onClick={navigateToTop}>
         ＜
       </button>
+      <button onClick={handleDelete}>削除</button>
     </>
   );
 };
