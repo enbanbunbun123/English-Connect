38168e97f928d651d5e63c88610dea47eac262ad
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-17 01:01:02 +0900

    feat: ItemDetailページから投稿の削除を可能に

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -1,10 +1,27 @@
 import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
-import { getDatabase, ref, remove } from "firebase/database";
+import { getDatabase, off, onValue, ref, remove } from "firebase/database";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
 
 const ItemDetail: React.FC = () => {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
+  const [postUserId, setPostUserId] = useState<string | null>(null);
+  const currentUserId = auth.currentUser?.uid;
+
+  useEffect(() => {
+    const db = getDatabase();
+    const postRef = ref(db, `posts/${id}`);
+    const listener = onValue(postRef, (snapshot) => {
+      const data = snapshot.val();
+      setPostUserId(data?.userId || null);
+    });
+
+    return () => {
+      off(postRef, "value", listener);
+    };
+  }, [id]);
 
   const navigateToTop = () => {
     navigate("/");
@@ -28,7 +45,9 @@ const ItemDetail: React.FC = () => {
       <button className="PostForm__back-button" onClick={navigateToTop}>
         ＜
       </button>
-      <button onClick={handleDelete}>削除</button>
+      {currentUserId === postUserId && (
+        <button onClick={handleDelete}>削除</button>
+      )}
     </>
   );
 };
