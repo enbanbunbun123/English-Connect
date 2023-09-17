b1df9e3d6fc3a069bb06e0258a8f713b1ff52f43
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-17 21:14:39 +0900

    feat; cardコンポーネントにデータを表示

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import { getDatabase, off, onValue, ref, remove } from "firebase/database";
 import { useEffect, useState } from "react";
 import { auth } from "../firebase";
+import "../stylesheet/itemDetail.scss";
 
 const ItemDetail: React.FC = () => {
   const navigate = useNavigate();
@@ -10,11 +11,22 @@ const ItemDetail: React.FC = () => {
   const [postUserId, setPostUserId] = useState<string | null>(null);
   const currentUserId = auth.currentUser?.uid;
 
+  const [postData, setPostData] = useState<{
+    userId?: string;
+    userName?: string;
+    userImage?: string;
+    text?: string;
+    timestamp?: string;
+    startData?: string;
+    postDescription?: string;
+  } | null>(null);
+
   useEffect(() => {
     const db = getDatabase();
     const postRef = ref(db, `posts/${id}`);
     const listener = onValue(postRef, (snapshot) => {
       const data = snapshot.val();
+      setPostData(data);
       setPostUserId(data?.userId || null);
     });
 
@@ -45,6 +57,13 @@ const ItemDetail: React.FC = () => {
       <button className="PostForm__back-button" onClick={navigateToTop}>
         ＜
       </button>
+      <div className="ItemDetail__contents">
+        <h3>{postData?.text}</h3>
+        <p>説明: {postData?.postDescription}</p>
+        <p>投稿者: {postData?.userName}</p>
+        <p>作成日: {new Date(postData?.timestamp || "").toLocaleString()}</p>
+        <p>開始日: {postData?.startData}</p>
+      </div>
       {currentUserId === postUserId && (
         <button onClick={handleDelete}>削除</button>
       )}
