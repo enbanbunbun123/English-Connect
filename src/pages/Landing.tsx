357182b6c0818f7db1e78d3c3dedf9fdcd62abd3
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-17 23:21:01 +0900

    fix: landingページの文言修正

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,7 +5,7 @@ const Landing: React.FC = () => {
   return (
     <>
       <div className="Landing">
-        <div className="Landing__title">Welcome to English Connect!</div>
+        <div className="Landing__title">Welcome to Event Search!</div>
         <SignInButton />
       </div>
     </>
