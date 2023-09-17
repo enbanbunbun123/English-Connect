5881ffc93403e0da1db0594049944074a7950204
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-17 10:50:24 +0900

    feat: cardに残り時間のカウントダウンを表示

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import "../stylesheet/card.scss";
+import { useEffect, useState } from "react";
 
 type CardProps = {
   userName: string;
@@ -17,9 +18,36 @@ const Card: React.FC<CardProps> = ({
   startData,
 }) => {
   const navigate = useNavigate();
+  const [timeLeft, setTimeLeft] = useState<number>(0);
+
+  useEffect(() => {
+    const startTime = new Date(startData).getTime();
+    const updateCountDown = () => {
+      const now = new Date().getTime();
+      const distance = startTime - now;
+      setTimeLeft(distance);
+    };
+
+    updateCountDown();
+    const interval = setInterval(updateCountDown, 1000);
+
+    return () => clearInterval(interval);
+  }, [startData]);
 
   const navigateToItemDetail = () => {
     navigate(`/item-detail/${id}`);
+  };
+
+  const formatTimeLeft = () => {
+    if (timeLeft < 0) return "イベント終了";
+
+    const hours = Math.floor(
+      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    return `開始まで${hours}h ${minutes}m ${seconds}s`;
   };
 
   return (
@@ -28,7 +56,7 @@ const Card: React.FC<CardProps> = ({
         <h3 className="card__title">{text}</h3>
         <p className="card__text">{userName}</p>
         <div>作成日 : {new Date(timestamp).toLocaleString()}</div>
-        <div>開始日 : {startData}</div>
+        <div>{formatTimeLeft()}</div>
       </div>
     </>
   );
