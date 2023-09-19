df9eed1bee67f8ef6a74765e233a1a2c76eabb7c
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-19 22:31:59 +0900

    feat: my-pageで名前と画像の変更を可能に

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,9 +1,55 @@
+import { getAuth, updateProfile } from "firebase/auth";
 import BackToHomeButton from "../components/BackToHomeButton";
+import { useState } from "react";
 
 const MyPage = () => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [displayName, setDisplayName] = useState<string>(
+    user?.displayName || ""
+  );
+  const [photoURL, setPhotoURL] = useState<string>(user?.photoURL || "");
+
+  const handleEdit = () => [setIsEditing(true)];
+
+  const handleSave = async () => {
+    if (user) {
+      await updateProfile(user, {
+        displayName: displayName,
+        photoURL: photoURL,
+      });
+      setIsEditing(false);
+    }
+  };
+
   return (
     <>
       <BackToHomeButton />
+      <div className="MyPage">
+        {isEditing ? (
+          <>
+            <input
+              value={displayName}
+              onChange={(e) => setDisplayName(e.target.value)}
+              placeholder="名前を入力"
+            />
+            <input
+              value={photoURL}
+              onChange={(e) => setPhotoURL(e.target.value)}
+              placeholder="画像のURLを入力"
+            />
+            <button onClick={handleSave}>編集完了</button>
+          </>
+        ) : (
+          <>
+            <img src={photoURL} alt="ユーザーの写真" />
+            <p>{displayName}</p>
+            <button onClick={handleEdit}>編集</button>
+          </>
+        )}
+      </div>
     </>
   );
 };
