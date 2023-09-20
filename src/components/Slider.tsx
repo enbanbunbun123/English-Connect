57ab305617bb5f73803793de182ed7bdb9f32b55
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-21 01:04:58 +0900

    feat: landingページにsliderを追加

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,35 +1,46 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/scrollbar";
 
 import "../stylesheet/slider.scss";
-
-import SlickSlider from "react-slick";
 
 const Slider: React.FC = () => {
   const images = [
     "images/event1.jpg",
+    "images/event2.jpg",
+    "images/event2.jpg",
+    "images/event2.jpg",
+    "images/event2.jpg",
+    "images/event2.jpg",
+    "images/event1.jpg",
+    "images/event1.jpg",
     "images/event1.jpg",
     "images/event1.jpg",
   ];
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidestoScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-  };
+
   return (
     <>
       <div className="Slider">
-        <SlickSlider {...settings}>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={5}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
           {images.map((image, index) => (
-            <div className="Slider__image" key={index}>
-              <img src={image} alt={`${index + 1}`} />
-            </div>
+            <SwiperSlide key={index}>
+              <img className="Slider__image" src={image} alt={`${index + 1}`} />
+            </SwiperSlide>
           ))}
-        </SlickSlider>
+        </Swiper>
       </div>
     </>
   );
