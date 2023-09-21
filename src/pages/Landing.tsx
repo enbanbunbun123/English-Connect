b91e512a57938a38dce3c1732feebae194003a75
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-21 18:19:05 +0900

    feat: Landingページ刷新

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -6,9 +6,21 @@ const Landing: React.FC = () => {
   return (
     <>
       <div className="Landing">
-        <div className="Landing__title">Welcome to Event Search!</div>
-        {/* <Slider /> */}
-        <SignInButton />
+        <div className="Landing__wrapper">
+          <img className="Landing__image" src="images/event2.jpg" alt=""></img>
+          {/* <Slider /> */}
+          <div className="Landing__overlay">
+            <div className="Landing__text">
+              <div className="Landing__text__title">
+                Event Search へようこそ！
+              </div>
+              <div className="Landing__text__contents">
+                PC,スマートフォンでいつでもどこでも全国のイベントを投稿・検索できます。あなたも始めてみませんか？
+              </div>
+            </div>
+            <SignInButton />
+          </div>
+        </div>
       </div>
     </>
   );
