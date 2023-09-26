cbf40697ea6cb25b6cadf6547b4b00a46f21a1b9
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-26 12:09:13 +0900

    feat: ランキングページ実装

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -1,7 +1,15 @@
+import BackToHomeButton from "../components/BackToHomeButton";
+import "../stylesheet/ranking.scss";
+
 const Ranking: React.FC = () => {
   return (
     <>
-      <></>
+      <div className="Ranking">
+        <BackToHomeButton />
+        <div className="Ranking__contents">
+          <div className="Ranking__contents__title">ランキング</div>
+        </div>
+      </div>
     </>
   );
 };
