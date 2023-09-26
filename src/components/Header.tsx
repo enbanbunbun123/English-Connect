c93a500f725943bc158b84436fd3a2016c347ea7
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-09-26 12:17:29 +0900

    fix: ランキングページをコメントアウト

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,9 +52,9 @@ const Header: React.FC = () => {
                   <div>マイページ</div>
                 </Link>
               )}
-              <Link className="Header__left__menu" to={`/ranking`}>
+              {/* <Link className="Header__left__menu" to={`/ranking`}>
                 <div>ランキング</div>
-              </Link>
+              </Link> */}
             </>
           )}
         </div>
@@ -107,13 +107,13 @@ const Header: React.FC = () => {
                     <div>マイページ</div>
                   </Link>
                 )}
-                <Link
+                {/* <Link
                   className="Header__modal-menu__menu"
                   to={`/ranking`}
                   onClick={() => setIsOpen(false)}
                 >
                   <div>ランキング</div>
-                </Link>
+                </Link> */}
                 <SignOutButton />
               </>
             )}
