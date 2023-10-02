fa0226bdf68c395ff5f72d6d5ba43538466b42f4
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-02 16:40:40 +0900

    fix: サインインページ遷移のバグを修正

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,28 @@ import PostForm from "./pages/PostForm";
 import ItemDetail from "./pages/ItemDetail";
 import MyPage from "./pages/MyPage";
 import Header from "./components/Header";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 import Footer from "./components/Footer";
 import Ranking from "./pages/Ranking";
+import Landing from "./pages/Landing";
 import SignIn from "./pages/SignIn";
 
 const App: React.FC = () => {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <Header />
+      {user && <Header />}
       <div className="App__content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/sign-in" element={<SignIn />} />
           <Route path="/post-form" element={<PostForm />} />
           <Route path="/item-detail/:id" element={<ItemDetail />} />
           <Route path="/my-page/:userId" element={<MyPage />} />
           <Route path="/ranking" element={<Ranking />} />
-          <Route path="/sign-in" element={<SignIn />} />
         </Routes>
       </div>
       <Footer />
--- src/components/SignInGuest.tsx
+++ src/components/SignInGuest.tsx
@@ -1,12 +1,16 @@
 import { signInAnonymously } from "firebase/auth";
 import { auth } from "../firebase";
 import "../stylesheet/signInGuest.scss";
+import { useNavigate } from "react-router-dom";
 
 const SignInGuest: React.FC = () => {
+  const navigate = useNavigate();
+
   const signInAsGuest = () => {
     signInAnonymously(auth)
       .then(() => {
         console.log("Signed in as a guest successfully");
+        navigate("/");
       })
       .catch((error) => {
         console.log("Error signing in as a guest: ", error);
